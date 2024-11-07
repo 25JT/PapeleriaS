278b4fc611bc50caeb1e88ab662b78617a596a21
25JT <jhonierlucumimeza@gmial.com>
2024-11-07 16:06:22 -0500

    ver1.0.2 Actualizacion del icono y se le agrega funcionaliddad falta diseñar el css del boton

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-//Movimiento del carusel
+//Icono Funcion
+function mesj() {
+  const numeroTelefono = "573014414701"// Escribe el numero sin el + del pais ejemplo 57325426421852154
+  const mensaje = document.getElementById('message-text').value
+  const icono = document.getElementById('fixed-icon')
+  new bootstrap.Modal(document.getElementById('exampleModal'))
 
+  if (mensaje.trim() === "") {
+      icono.style.display = 'none';
+      Swal.fire({
+          icon: "warning",
+          title: "Oops...",
+          text: "Por favor escribe en el campo",
+      }).then(() => {
+          icono.style.display = 'block';
+      });
+  } else {
+      const url = `https://wa.me/${numeroTelefono}?text=${encodeURIComponent(mensaje)}`;
+      window.open(url, "_blank");
+      mensaje = document.getElementById('message-text').value = " "
+      modal.hide()
+  }
+}
+
+//Movimiento del carusel
 document.addEventListener('DOMContentLoaded', (event) => {
     var carouselElement = document.querySelector('#carouselExampleRide');
     var carousel = new bootstrap.Carousel(carouselElement, {
@@ -56,4 +79,54 @@ document.addEventListener('DOMContentLoaded', (event) => {
         colDiv.appendChild(cardDiv);
         container.appendChild(colDiv);
     });
+});
+//index papeleria
+document.addEventListener("DOMContentLoaded", function () {
+  const products = [
+    { title: "Borradores", price: "$400", imgSrc: "Img/Papeleria/borrador.png", link: "https://wa.me/message/Q4IDJZDEGDYYO1" },
+    { title: "Cuaderno", price: "$4.200", imgSrc: "Img/Papeleria/cuaderno.jpg", link: "https://wa.me/message/Q4IDJZDEGDYYO1" },
+    { title: "Marcadores", price: "$2.500", imgSrc: "Img/Papeleria/marcadores.jpg", link: "https://wa.me/message/Q4IDJZDEGDYYO1" },
+    { title: "Fomi 1/8 paquete diversos colores", price: "$600", imgSrc: "Img/Papeleria/fomi.png", link: "https://wa.me/message/Q4IDJZDEGDYYO1" },
+    { title: "Lapiceros en azul negro y rojo", price: "$1.200", imgSrc: "Img/Papeleria/lapicero-de-punta-fina-0ffi-Esco-vertical.jpg", link: "https://wa.me/message/Q4IDJZDEGDYYO1" },
+    { title: "Ega en barra", price: "$2.400", imgSrc: "Img/Papeleria/ega en barra.png", link: "https://wa.me/message/Q4IDJZDEGDYYO1" }
+  ];
+
+  const container = document.getElementById("product-container");
+
+  products.forEach(product => {
+    const colDiv = document.createElement("div");
+    colDiv.className = "col-md-4 mb-5";
+
+    const cardDiv = document.createElement("div");
+    cardDiv.className = "card tarjeta";
+
+    const link = document.createElement("a");
+    link.href = product.link;
+    link.target = "_blank ";
+
+    const img = document.createElement("img");
+    img.className = "card-img-top ";
+    img.src = product.imgSrc;
+    img.alt = product.title;
+
+    const cardBody = document.createElement("div");
+    cardBody.className = "card-body text-center";
+
+
+    const cardTitle = document.createElement("h5");
+    cardTitle.className = "card-title";
+    cardTitle.innerText = product.title;
+
+    const cardText = document.createElement("p");
+    cardText.className = "card-text";
+    cardText.innerText = product.price;
+
+    link.appendChild(img);
+    cardDiv.appendChild(link);
+    cardDiv.appendChild(cardBody);
+    cardBody.appendChild(cardTitle);
+    cardBody.appendChild(cardText);
+    colDiv.appendChild(cardDiv);
+    container.appendChild(colDiv);
+  });
 });
