73b1fa832ee9c98ad65ca8693cd6b84783be5e10
25JT <jhonierlucumimeza@gmail.com>
2026-01-11 11:28:04 -0500

    Actualizacion de alerta

--- scripts/script.js
+++ scripts/script.js
@@ -6,38 +6,21 @@ function mesj() {
   new bootstrap.Modal(document.getElementById('exampleModal'))
 
   if (mensaje.trim() === "") {
-      icono.style.display = 'none';
-      Swal.fire({
-          icon: "warning",
-          title: "Oops...",
-          text: "Por favor escribe en el campo",
-          confirmButtonColor:"#e96e93",
-      }).then(() => {
-          icono.style.display = 'block';
-      });
+    icono.style.display = 'none';
+    Swal.fire({
+      icon: "warning",
+      title: "Oops...",
+      text: "Por favor escribe en el campo",
+      confirmButtonColor: "#e96e93",
+    }).then(() => {
+      icono.style.display = 'block';
+    });
   } else {
-      const url = `https://wa.me/${numeroTelefono}?text=${encodeURIComponent(mensaje)}`;
-      window.open(url, "_blank");
-      mensaje = document.getElementById('message-text').value = " "
-      modal.hide()
+    const url = `https://wa.me/${numeroTelefono}?text=${encodeURIComponent(mensaje)}`;
+    window.open(url, "_blank");
+    mensaje = document.getElementById('message-text').value = " "
+    modal.hide()
   }
 }
-
-function BienvenidaAlerta(params) {
-    
-    Swal.fire({
-        title: "GRACIAS POR VISITARNOS",
-        text: "Esta pagina se encuntra en formato pre-lanzamiento por lo que algunos servicios pueden no estar disponibles. porfavor informanos de cualquier error.😊",
-        icon: "info",
-        width: 600,
-        padding: "3em",
-        color: "#000000",
-        backdrop: `
-          rgb(0, 0, 0, 0.900)
-        `,
-        confirmButtonColor: "#e03166",
-      });
-}
-
 
 
